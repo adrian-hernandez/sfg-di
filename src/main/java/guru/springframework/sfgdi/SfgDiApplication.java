d56ee95b50428afd849e20f6535402462c530a16
adrian-hernandez <1hernandezadrian1@gmail.com>
2021-02-11 20:48:00 -0800

    Add property, setter, and constructor based DI examples

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,13 +1,28 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		// This returns an application context
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+
+		// Ask context for an instance of my controller
+		// "Spring, from the context please provide me with an instance of this Bean"
+		// This is one of the fundamentals of dependency injection ; having the framework do the management of it
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		// Important : notice that we never created the controller object i.e never did New MyController()
+		// Spring is doing that for us
+		// Under the cover Spring is creating that inside the Spring Context
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyInjectedControllerTest.java
@@ -0,0 +1,28 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+// Property Injection example
+// Not the most popular but still go to know it
+// We mimic what Spring Framework does under the hood
+class PropertyInjectedControllerTest {
+
+    PropertyInjectedController controller;
+
+    @BeforeEach
+    void setUp(){
+        controller = new PropertyInjectedController();
+
+        controller.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    void getGreeting() {
+
+        System.out.println(controller.getGreeting());
+    }
+}
